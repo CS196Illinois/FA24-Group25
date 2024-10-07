d2aacc5e45419dd755d6ed3df0ee72a92a0559a0
theinvisibletree <stevenszwang@gmail.com>
2024-10-07 16:12:19 -0500

    added paddle dash and pull + basic menu

--- Project/pong.py
+++ Project/pong.py
@@ -2,48 +2,75 @@ class Ball:
     def __init__(self, posX, posY, velX, velY, width, height, image):
         self.velX = velX
         self.velY = velY
+        self.width = width
+        self.height = height
         self.sprite = pygame.transform.scale(pygame.image.load(image).convert(), (width,height))
         self.rect = self.sprite.get_rect(center = (posX, posY))
+        self.lastHit = None
         ballList.append(self)
 
     def checkCollision(self):
         if self.rect.top < 0 or self.rect.bottom > resY:
             self.velY *= -1
         if self.rect.collideobjects(paddleList) != None:
-            self.velX *= -1.1
+            self.lastHit = self.rect.collideobjects(paddleList)
+            self.velX *= -1.05
             self.velY += (self.rect.centery - self.rect.collideobjects(paddleList).rect.centery) * .03
     
     def updatePos(self):
         self.rect.centerx += self.velX
         self.rect.centery += self.velY
+        if self.lastHit != None and keys[self.lastHit.keyPull]:
+            if self.lastHit.rect.centery > self.rect.centery:
+                self.velY += .05
+            if self.lastHit.rect.centery < self.rect.centery:
+                self.velY -= .05
 
     def reset(self, posX, posY, velX, velY):
         self.velX = velX
         self.velY = velY
         self.rect.center = (posX,posY)
+        self.lastHit = None
 
     def display(self):
         screen.blit(self.sprite, self.rect)
-
-
+        if self.lastHit and keys[self.lastHit.keyPull]:
+            self.sprite = pygame.transform.scale(pygame.image.load('graphics/' + self.lastHit.name + 'ball.png').convert(), (self.width,self.height))
+        else:
+            self.sprite = pygame.transform.scale(pygame.image.load('graphics/ball.png').convert(), (self.width,self.height))
 class Paddle:
-    def __init__(self, posX, posY, width, height, speed, keyUp, keyDown, image):
+    def __init__(self, posX, posY, width, height, speed, dashCooldownSeconds, keyUp, keyDown, keyDash, keyPull, name):
         self.speed = abs(speed)
-        self.sprite = pygame.transform.scale(pygame.image.load(image).convert(), (width,height))
+        self.width = width
+        self.height = height
+        self.sprite = pygame.transform.scale(pygame.image.load('graphics/' + name + '.png').convert(), (width,height))
         self.rect = self.sprite.get_rect(center = (posX,posY))
         self.keyUp = keyUp
         self.keyDown = keyDown
+        self.keyDash = keyDash
+        self.keyPull = keyPull
+        self.dashCooldownSeconds = dashCooldownSeconds
+        self.timeSinceDash = dashCooldownSeconds
+        self.name = name
         paddleList.append(self)
 
     def getRect(self):
         return self.rect
 
     def updatePos(self):
-        keys = pygame.key.get_pressed()
+        self.timeSinceDash += 1/60
         if keys[self.keyUp]:
             self.rect.centery -= self.speed
         if keys[self.keyDown]:
             self.rect.centery += self.speed
+        if self.timeSinceDash >= self.dashCooldownSeconds:
+            if keys[self.keyDash]:
+                if keys[self.keyUp]:
+                    self.timeSinceDash = 0
+                    self.rect.centery -= self.speed * 25
+                elif keys[self.keyDown]:
+                    self.timeSinceDash = 0
+                    self.rect.centery += self.speed * 25
         if self.rect.top < 0:
             self.rect.top = 0
         elif self.rect.bottom > resY:
@@ -51,8 +78,19 @@ class Paddle:
 
     def display(self):
         screen.blit(self.sprite, self.rect)
+        if self.timeSinceDash < self.dashCooldownSeconds:
+            if keys[self.keyPull]:
+                self.sprite = pygame.transform.scale(pygame.image.load('graphics/' + self.name + 'cooldown.png').convert(), (self.width,self.height))
+            else:
+                self.sprite = pygame.transform.scale(pygame.image.load('graphics/paddlecooldown.png').convert(), (self.width,self.height))
+        else:
+            if keys[self.keyPull]:
+                self.sprite = pygame.transform.scale(pygame.image.load('graphics/' + self.name + '.png').convert(), (self.width,self.height))
+            else:
+                self.sprite = pygame.transform.scale(pygame.image.load('graphics/paddle.png').convert(), (self.width,self.height))
 
 import pygame
+import random
 from sys import exit
 
 pygame.init()
@@ -70,50 +108,64 @@ pygame.display.set_caption('pong')
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 50)
 
-background = pygame.image.load('graphics/background.png').convert()
-background = pygame.transform.scale(background, (resX,resY))
+title = pygame.transform.scale(pygame.image.load('graphics/title.png').convert(), (resX,resY))
+background = pygame.transform.scale(pygame.image.load('graphics/background.png').convert(), (resX,resY))
 
 ball = dict()
 for i in range(0,1):
-    ball[i] = Ball(resX * .5, resY * .5, 4, 0, 16, 16, 'graphics/ball.png')
+    ball[i] = Ball(resX * .5, resY * .5, -4, 0, resX * .017, resX * .017, 'graphics/ball.png')
 
-paddleL = Paddle(resX * .03, resY * .5, resX * .015, resY * .25, 6, pygame.K_w, pygame.K_s, 'graphics/paddle.png')
-paddleR = Paddle(resX * .97, resY * .5, resX * .015, resY * .25, 6, pygame.K_UP, pygame.K_DOWN, 'graphics/paddle.png')
+
+paddleL = Paddle(resX * .02, resY * .5, resX * .017, resY * .186, 6, 3, pygame.K_w, pygame.K_s, pygame.K_d, pygame.K_a, 'paddleL')
+paddleR = Paddle(resX * .98, resY * .5, resX * .017, resY * .186, 6, 3, pygame.K_UP, pygame.K_DOWN, pygame.K_RIGHT, pygame.K_LEFT, 'paddleR')
+
+start = False
 
 while True:
+    keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                start = True
+                scoreL = 0
+                scoreR = 0
+                for ball in ballList:
+                    ball.reset(resX * .5, resY * .5, -4, 0)
             if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                exit()
-    
+                start = False
+
     textL = font.render(str(scoreL), True, 'White')
     textRectL = textL.get_rect(center = (resX * .4, resY * .1))
     textR = font.render(str(scoreR), True, 'White')
     textRectR = textR.get_rect(center = (resX * .6, resY * .1))
 
-    screen.blit(background, (0,0))
-    screen.blit(textL, textRectL)
-    screen.blit(textR, textRectR)
+    if start == False:
+        screen.blit(title, (0,0))
+    else:
+        screen.blit(background, (0,0))
+        screen.blit(textL, textRectL)
+        screen.blit(textR, textRectR)
     
     for ball in ballList:
         ball.checkCollision()
-        if ball.rect.left < 0:
+        if ball.rect.left < resX * .02:
             scoreR += 1
             ball.reset(resX * .5, resY * .5, -4, 0)
-        if ball.rect.right > resX:
+        if ball.rect.right > resX * .98:
             scoreL += 1
             ball.reset(resX * .5, resY * .5, 4, 0)
     
+    for paddle in paddleList:
+        paddle.updatePos()
+
     for ball in ballList:
         ball.updatePos()
         ball.display()
     
     for paddle in paddleList:
-        paddle.updatePos()
         paddle.display()
 
     pygame.display.update()
