35b909ca2f4b85464c7d7e1951b4534233f509fc
Emily <emilykarb@gmail.com>
2024-10-18 13:57:14 -0500

    added changing colors, and changes in direction when hit by paddle

--- Project/emilyk7/project.py
+++ Project/emilyk7/project.py
@@ -10,6 +10,8 @@ pygame.init()
 #colors
 BLUE = (0,0,255)
 BLACK = (0,0,0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
 
 #Screen dimensions
 WIDTH = 800
@@ -18,6 +20,30 @@ HEIGHT = 500
 # Set up the drawing window
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 
+#fullscreen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+#screen = pygame.Surface((WIDTH, HEIGHT))
+
+#In main loop before display.flip():
+
+clock = pygame.time.Clock()
+fps = 50
+'''
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, xLoc, yLoc, width, color):
+        super().__init__()
+
+        self.image = pygame.Surface([width, width])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        pygame.draw.rect(self.image, color, pygame.Rect(0, 0, width, width))
+        self.rect = self.image.get_rect()
+        self.rect.x = xLoc
+        self.rect.y = yLoc
+
+        def draw(self, surface):
+            surface.blit(self.image, self.rect)
+'''
 class Ball(pygame.sprite.Sprite):
     def __init__(self, color, radius, xLoc, yLoc):
         super().__init__()
@@ -30,9 +56,9 @@ class Ball(pygame.sprite.Sprite):
         self.rect.x = float(xLoc)
         self.rect.y = float(yLoc)
 
-        self.dy = 0.6
-        self.dx = 0.7
-        #change the entire how we are moving the ball
+        self.dy = 0
+        self.dx = 5
+        #change the entire how we are moving the ball   
 
     def toMove(self):
         self.rect.x += float(self.dx)   # Update rect position horizontally
@@ -55,7 +81,7 @@ class Paddle(pygame.sprite.Sprite):
         self.rect.y = yLoc  # Set the initial y position of the paddle
         self.rect.x = xLoc
 
-        self.dy = 0.7  # Paddle movement speed
+        self.dy = 7  # Paddle movement speed
 
     def toMove(self):
         self.rect.y += self.dy  # Update rect position directly
@@ -66,28 +92,30 @@ class Paddle(pygame.sprite.Sprite):
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
-Paddle1 = Paddle(BLUE, 15, 70.0, 50.0, 20)
-Paddle2 = Paddle(BLUE, 15, 70.0, 50.0, 780)
-PingPong = Ball(BLUE, 5, 50.0, 50.0)
+Paddle1 = Paddle(BLUE, 15, 90.0, 218.0, 20)
+Paddle2 = Paddle(RED, 15, 90.0, 218.0, 760)
+PingPong = Ball(WHITE, 10, 350.0, 250.0)
+#Obstacle1 = Obstacle(200, 50, 10, WHITE)
 # Run until the user asks to quit
 running = True
 playerOneScore = 0
 playerTwoScore = 0
+playerBall = True #tells whose turn it is, true means player 2; false means player 1
 while running:
-
+    clock.tick(fps)
     # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     # Fill the background with white
-    screen.fill((255, 255, 255))
+    screen.fill((0, 0, 0))
 
     #Text and score on top
     text = str(playerOneScore) + " | " + str(playerTwoScore)
     font = pygame.font.SysFont(None, 30);
-    img = font.render(text, True, (0,0,255))
-    screen.blit(img, (400, 20))
+    img = font.render(text, True, WHITE)
+    screen.blit(img, (380, 20))
 
     # Ping pong ball and score keeping
     PingPong.toMove()
@@ -99,11 +127,17 @@ while running:
     if PingPong.rect.bottom > screen.get_height():
         PingPong.dy *= -1
     if PingPong.rect.left < 0:
-        PingPong.dx *= -1
+        PingPong.dx = -5
+        PingPong.dy = 0
         playerTwoScore += 1
+        PingPong.rect.x = 350
+        PingPong.rect.y = 250
     if PingPong.rect.right > screen.get_width():
-        PingPong.dx *= -1
+        PingPong.dx = 5
+        PingPong.dy = 0
         playerOneScore += 1
+        PingPong.rect.x = 350
+        PingPong.rect.y = 250
 
     #user inputs for both paddles
     keys = pygame.key.get_pressed()
@@ -127,16 +161,60 @@ while running:
         Paddle2.rect.bottom = screen.get_height()
     
     if pygame.sprite.collide_rect(Paddle1, PingPong):
+        distanceFromTop = Paddle1.rect.bottom - PingPong.rect.centery
+        if distanceFromTop <= 15:
+            PingPong.dy = 7
+        elif distanceFromTop <= 25:
+            PingPong.dy = 5
+        elif distanceFromTop <= 33:
+            PingPong.dy = 4
+        elif distanceFromTop <= 41:
+            PingPong.dy = 2
+        elif distanceFromTop <= 49:
+            PingPong.dy = 0
+        elif distanceFromTop <= 57:
+            PingPong.dy = -2
+        elif distanceFromTop <= 65:
+            PingPong.dy = -4
+        elif distanceFromTop <= 75:
+            PingPong.dy = -5
+        else:   
+            PingPong.dy = -7
         PingPong.dx *= -1
+        PingPong.image.fill(BLUE)
+        playerBall = False
 
     if pygame.sprite.collide_rect(PingPong, Paddle2):
+        distanceFromTop = Paddle2.rect.bottom - PingPong.rect.centery
+        if distanceFromTop <= 15:
+            PingPong.dy = 9
+        elif distanceFromTop <= 25:
+            PingPong.dy = 7
+        elif distanceFromTop <= 33:
+            PingPong.dy = 3
+        elif distanceFromTop <= 41:
+            PingPong.dy = 2
+        elif distanceFromTop <= 49:
+            PingPong.dy = 0
+        elif distanceFromTop <= 57:
+            PingPong.dy = -2
+        elif distanceFromTop <= 65:
+            PingPong.dy = -3
+        elif distanceFromTop <= 75:
+            PingPong.dy = -7
+        else:   
+            PingPong.dy = -9
         PingPong.dx *= -1
+        PingPong.image.fill(RED)
+        playerBall = True
     
-
     Paddle1.draw(screen)
     Paddle2.draw(screen)
     PingPong.draw(screen)
+    #Obstacle1.draw(screen)
 
+    #scaled = pygame.transform.smoothscale(screen, fullscreen.get_size())
+    #fullscreen.blit(scaled, (0, 0))
     # Flip the display
     pygame.display.flip()
 
