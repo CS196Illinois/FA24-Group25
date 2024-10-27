5c9c1920d9d9a0b45b18e2b5e089ae2fa1f06d74
Mariano Rodriguez <amr001.us@proton.me>
2024-10-27 17:27:42 -0500

    comment some things

--- Project/mrodr292_foosball.py
+++ Project/mrodr292_foosball.py
@@ -1,10 +1,5 @@
 import pygame
-from pygame.constants import (
-    K_UP,
-    K_DOWN,
-    K_s,
-    K_w
-)
+from pygame.constants import K_UP, K_DOWN, K_s, K_w
 
 from pong_common import GameState, Ball, Paddle, SCREEN_HEIGHT, SCREEN_WIDTH, SCORE
 
@@ -17,12 +12,14 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 running = True
 
 ball = Ball()
-player = Paddle(SCREEN_WIDTH/10, SCREEN_HEIGHT/2, (K_w, K_s), size=60)
-player2 = Paddle((SCREEN_WIDTH * 4)/10, SCREEN_HEIGHT/2, (K_UP, K_DOWN), size=60)
-cpu1 = Paddle((SCREEN_WIDTH * 9)/10, SCREEN_HEIGHT/2, (), size=30, thresh = 800)
-cpu2 = Paddle((SCREEN_WIDTH * 6)/10, SCREEN_HEIGHT/2, (), size=30, thresh= 500)
+player = Paddle(SCREEN_WIDTH / 10, SCREEN_HEIGHT / 2, (K_w, K_s), size=60)
+player2 = Paddle((SCREEN_WIDTH * 4) / 10, SCREEN_HEIGHT / 2, (K_UP, K_DOWN), size=60)
+cpu1 = Paddle((SCREEN_WIDTH * 9) / 10, SCREEN_HEIGHT / 2, (), size=30, thresh=800)
+cpu2 = Paddle((SCREEN_WIDTH * 6) / 10, SCREEN_HEIGHT / 2, (), size=30, thresh=500)
 score = pygame.font.Font(FONT, 20)
-score_text = score.render(f"{GameState.p1score} - {GameState.p2score}", False, (255, 255, 255))
+score_text = score.render(
+    f"{GameState.p1score} - {GameState.p2score}", False, (255, 255, 255)
+)
 
 clock = pygame.time.Clock()
 
@@ -42,16 +39,18 @@ paddles.add(cpu2)
 dt = 0
 
 while running:
-    screen.fill((0,128,0))
+    screen.fill((0, 128, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == SCORE:
-            score_text = score.render(f"{GameState.p1score} - {GameState.p2score}", False, (255, 255, 255))
+            score_text = score.render(
+                f"{GameState.p1score} - {GameState.p2score}", False, (255, 255, 255)
+            )
             for entity in all_sprites:
                 entity.reset()
-    
+
     ball.update(dt)
     ball.collide(paddles, dt)
     keys = pygame.key.get_pressed()
@@ -62,7 +61,7 @@ while running:
     for entity in all_sprites:
         screen.blit(entity.surf, entity.rect)
 
-    screen.blit(score_text, ((int(SCREEN_WIDTH/2), (SCREEN_HEIGHT * 9)/10)))
+    screen.blit(score_text, ((int(SCREEN_WIDTH / 2), (SCREEN_HEIGHT * 9) / 10)))
 
     pygame.display.flip()
-    dt = clock.tick(120)/1000.0
+    dt = clock.tick(120) / 1000.0
--- Project/pong_common.py
+++ Project/pong_common.py
@@ -6,6 +6,8 @@ SCORE = pygame.USEREVENT + 1
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
+
+# this class should handle game state, like scores, teams, etc
 class GameState:
     p1score = 0
     p2score = 0
@@ -21,21 +23,27 @@ class GameState:
         event = pygame.event.Event(SCORE)
         pygame.event.post(event)
 
+
+# all behavior that the ball should be defined in here. can be extended if need be
 class Ball(pygame.sprite.Sprite):
+    # Todo: update ball constructor to take size (and consider other useful additions)
     def __init__(self):
         super(Ball, self).__init__()
 
         self.surf = pygame.Surface((15, 15))
 
         self.surf.fill((255, 255, 255))
-        self.rect = self.surf.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+        self.rect = self.surf.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
 
         self.speed = 500.0
+        # not sure how you want to implement acceleration Sofia but I think that perhaps each hit with a paddle should
+        # set ball speed to a max and then have the ball deccelerate to an arbitrary min
         self.angle = math.pi + random.randint(0, 1) * math.pi
         self.x_vel = self.speed * math.cos(self.angle)
         self.y_vel = self.speed * math.sin(self.angle)
         self.locked = False
 
+    # dt is time since last frame
     def update(self, dt):
         self.rect.move_ip(self.x_vel * dt, self.y_vel * dt)
         if self.rect.left < 0:
@@ -55,29 +63,40 @@ class Ball(pygame.sprite.Sprite):
     def collide(self, paddles, dt):
         for i in paddles:
             if i.rect.colliderect(self.rect):
-                offset = (self.rect.y + self.rect.height - i.rect.y) / (i.rect.height + self.rect.height)
+                # ratio of difference in height between paddle center and ball center difference and sum
+                offset = (self.rect.y + self.rect.height - i.rect.y) / (
+                    i.rect.height + self.rect.height
+                )
                 self.x_vel *= -1
-                self.rect.move_ip(self.x_vel*dt, -self.y_vel*dt)
+                self.rect.move_ip(self.x_vel * dt, -self.y_vel * dt)
                 phi = 0.3 * math.pi * (2 * offset - 1)
-                self.y_vel= self.speed * math.sin(phi)
+                self.y_vel = self.speed * math.sin(phi)
                 self.angle = math.atan2(self.y_vel, self.x_vel)
-                self.speed *= 1.01
+                self.speed *= 1.0 + 0.01
+
+    # figured this might be useful boilerplate
+    def set_speed(self, speed: int):
+        pass
 
     def reset(self):
         self.speed = 500
         self.angle = math.pi + random.randint(0, 1) * math.pi
-        self.rect.centerx = int(SCREEN_WIDTH/2)
-        self.rect.centery = int(SCREEN_HEIGHT/2)
+        self.rect.centerx = int(SCREEN_WIDTH / 2)
+        self.rect.centery = int(SCREEN_HEIGHT / 2)
 
+
+# all behaviour regarding paddle should be encased in this class
 class Paddle(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos, controls, size=75, thresh = 700):
+    # controls is a tuple of 2 controls that represent the key scancodes that will be used for controls,
+    # thresh is the ball x threshold for the AI to kick in
+    def __init__(self, x_pos, y_pos, controls, size=75, thresh=700):
         super(Paddle, self).__init__()
 
         self.surf = pygame.Surface((10, size))
 
-        self.surf.fill((255,255,255))
+        self.surf.fill((255, 255, 255))
 
-        self.rect = self.surf.get_rect(center = (x_pos, y_pos))
+        self.rect = self.surf.get_rect(center=(x_pos, y_pos))
         self.vel = 0.0
         self.initial_y = y_pos
         self.max_vel = 500.0
@@ -86,6 +105,7 @@ class Paddle(pygame.sprite.Sprite):
         self.locked = False
         self.thresh = thresh
 
+    # dt is time since last frame
     def update(self, pressed_keys, bally, ballx, dt):
         if len(self.controls) == 0 and ballx > self.thresh and ballx < self.rect.x:
             # Make overshoots possible on the part of the CPU
