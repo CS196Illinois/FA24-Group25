6df1a491bea059cb09517acda8d8dd7b2c75ea55
christiantaylor404 <cmt8@illinois.edu>
2024-12-03 22:04:02 -0600

    changed main.py to feature setting the screen to black at the start of each frame

--- Project/main.py
+++ Project/main.py
@@ -43,6 +43,7 @@ font = pygame.font.Font(FONT, 20)
 running = True
 
 while running:
+    settings.screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
